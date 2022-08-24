6db613372742780cc18de9f1d28fc5f876927d81
Abd Fahad <abdullahfahad925@gmail.com>
2022-08-25 00:39:08 +0500

    Delete note implemented

--- popup.js
+++ popup.js
@@ -58,6 +58,24 @@ document.addEventListener("DOMContentLoaded",()=>{
             matchedDelele[i].addEventListener("click",(event)=>{
                 let id = event.currentTarget.id;
                 console.log(id);
+
+                //Code from dcode@youtube 
+                Confirm.open({
+                    title: 'Delete Note',
+                    message: 'Are you sure you want to delete the note?',
+                    onok: () => {
+                       id = parseInt(id.slice(6));
+                        let nts = [];
+                        chrome.storage.sync.get('notes',(obj)=>{
+                            nts = [...obj.notes];
+                            nts.splice(id,1);
+                            chrome.storage.sync.set({'notes':nts});
+                            console.log(nts);
+                            location.href = "popup.html";
+                        })
+                    }
+                  })
+                //Code from dcode@youtube 
             });
         };
 
@@ -71,3 +89,75 @@ document.addEventListener("DOMContentLoaded",()=>{
     });
 });
 
+
+
+//Code from dcode@youtube 
+
+const Confirm = {
+    open (options) {
+        options = Object.assign({}, {
+            title: '',
+            message: '',
+            okText: 'OK',
+            cancelText: 'Cancel',
+            onok: function () {},
+            oncancel: function () {}
+        }, options);
+        
+        const html = `
+            <div class="confirm">
+                <div class="confirm__window">
+                    <div class="confirm__titlebar">
+                        <span class="confirm__title">${options.title}</span>
+                        <button class="confirm__close">&times;</button>
+                    </div>
+                    <div class="confirm__content">${options.message}</div>
+                    <div class="confirm__buttons">
+                        <button class="confirm__button confirm__button--ok confirm__button--fill">${options.okText}</button>
+                        <button class="confirm__button confirm__button--cancel">${options.cancelText}</button>
+                    </div>
+                </div>
+            </div>
+        `;
+
+        const template = document.createElement('template');
+        template.innerHTML = html;
+
+        // Elements
+        const confirmEl = template.content.querySelector('.confirm');
+        const btnClose = template.content.querySelector('.confirm__close');
+        const btnOk = template.content.querySelector('.confirm__button--ok');
+        const btnCancel = template.content.querySelector('.confirm__button--cancel');
+
+        confirmEl.addEventListener('click', e => {
+            if (e.target === confirmEl) {
+                options.oncancel();
+                this._close(confirmEl);
+            }
+        });
+
+        btnOk.addEventListener('click', () => {
+            options.onok();
+            this._close(confirmEl);
+        });
+
+        [btnCancel, btnClose].forEach(el => {
+            el.addEventListener('click', () => {
+                options.oncancel();
+                this._close(confirmEl);
+            });
+        });
+
+        document.body.appendChild(template.content);
+    },
+
+    _close (confirmEl) {
+        confirmEl.classList.add('confirm--close');
+
+        confirmEl.addEventListener('animationend', () => {
+            document.body.removeChild(confirmEl);
+        });
+    }
+};
+
+//Code from dcode@youtube 
