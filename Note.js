bfeed3782689f534d6161f70cfc35fd937214125
Abd Fahad <abdullahfahad925@gmail.com>
2022-08-30 18:18:44 +0500

    Context Menu and Badge

--- Note.js
+++ Note.js
@@ -1,9 +1,7 @@
 
 document.addEventListener("DOMContentLoaded",()=>{
 
-document.getElementById("discard").addEventListener("click",()=>{
-    location.href = "popup.html";
-});
+
 var replace = NaN;
 chrome.storage.sync.get('isEdit',(obj)=>{
     if(obj.isEdit.edit){
@@ -13,11 +11,13 @@ chrome.storage.sync.get('isEdit',(obj)=>{
             document.querySelector('#Body textarea').value = obj2.notes[obj.isEdit.id].body;
             replace = obj.isEdit.id;
             chrome.storage.sync.set({'isEdit':{edit:false, id:0}});
-        })
-        
+        });
         
     }
-})
+});
+
+
+console.log(document.querySelector('textarea').value.length);
 
 document.querySelector('textarea').addEventListener('keyup',()=> {
     
@@ -28,32 +28,76 @@ document.querySelector('textarea').addEventListener('keyup',()=> {
       
     console.log("Hello")
     current.innerHTML = characterCount;
-
 });
 
-document.getElementById("save").addEventListener("click",( )=>{
+chrome.storage.sync.get('selected',(obj3)=>{
+    if(obj3.selected){
+        document.querySelector('#Body textarea').value = obj3.selected;
+        document.querySelector('#current').innerHTML = document.querySelector('textarea').value.length;
+        chrome.storage.sync.set({'selected':''});
 
-    let title = document.querySelector('#title input').value || 'No title';
-    let body = document.querySelector('#Body textarea').value || 'Oops such empty';
 
-    console.log(title);
-    console.log(body);
-    let notes_arr = [];
-    chrome.storage.sync.get('notes',(obj)=>{
-        if(obj.notes){
-            notes_arr = [...obj.notes];
-        };
+        document.getElementById("save").addEventListener("click",( )=>{
+            let title = document.querySelector('#title input').value || 'No title';
+            let body = document.querySelector('#Body textarea').value || 'Oops such empty';
+            let notes_arr = [];
+            chrome.storage.sync.get('notes',(obj)=>{
+                if(obj.notes){
+                    notes_arr = [...obj.notes];
+                };
+                
+                notes_arr.push({title:title,body:body});
+        
+                chrome.storage.sync.set({'notes':notes_arr});
+                console.log(notes_arr);
+                
+                window.close();
+            });
+        
+            
+        });
 
-        if(isNaN(replace)){
-            notes_arr.push({title:title,body:body});
-        }else{
-            notes_arr[replace] = {title:title,body:body};
-        };
 
-        chrome.storage.sync.set({'notes':notes_arr});
-        console.log(notes_arr);    
-    });
+        document.getElementById("discard").addEventListener("click",()=>{
+            window.close();
+        });
 
-    location.href = "popup.html";    
+    }else{
+        document.getElementById("save").addEventListener("click",( )=>{
+
+            let title = document.querySelector('#title input').value || 'No title';
+            let body = document.querySelector('#Body textarea').value || 'Oops such empty';
+            document.querySelector('#current').innerHTML = document.querySelector('textarea').value.length;
+            console.log(title);
+            console.log(body);
+            let notes_arr = [];
+            chrome.storage.sync.get('notes',(obj)=>{
+                if(obj.notes){
+                    notes_arr = [...obj.notes];
+                };
+        
+                if(isNaN(replace)){
+                    notes_arr.push({title:title,body:body});
+                }else{
+                    notes_arr[replace] = {title:title,body:body};
+                };
+        
+                chrome.storage.sync.set({'notes':notes_arr});
+                console.log(notes_arr);    
+            });
+        
+            location.href = "popup.html";    
+        });
+
+        document.getElementById("discard").addEventListener("click",()=>{
+            location.href = "popup.html";
+        });
+
+    }
+   
 });
+
+
+
+
 });
--- eventPage.js
+++ eventPage.js
@@ -1,6 +1,37 @@
-// let notes = [ {'note1':'body1'}];
+const contextMenuItem = {
+    "id":"addToNotes",
+    "title":"Add to Notes",
+    "contexts":["selection"]
+};
 
-// chrome.runtime.onInstalled.addEventListener(()=>{
-//     chrome.storage.sync.set({notes});
-//     console.log(notes);
-// })
+chrome.storage.sync.get('notes',(obj)=>{
+    if(obj.notes){
+        let NoNotes = obj.notes.length;
+        chrome.action.setBadgeText({text: NoNotes.toString()});
+    }
+});
+
+chrome.storage.onChanged.addListener((changes, area)=>{
+    console.log(changes)
+    if( area == 'sync' && changes.notes){
+        console.log(changes);
+        chrome.action.setBadgeText({text: changes.notes.newValue.length.toString()});
+    };
+});
+
+
+chrome.contextMenus.create(contextMenuItem, ()=>{  
+
+
+chrome.contextMenus.onClicked.addListener((textData)=>{
+    if(textData.menuItemId == "addToNotes" && textData.selectionText && textData.selectionText.length <= 500){
+        console.log(textData.selectionText);
+        chrome.storage.sync.set({'selected':textData.selectionText},()=>{
+            chrome.tabs.create({url: 'Note.html'});
+        });
+        
+    }
+})
+
+
+});
